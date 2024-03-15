0e9dbe81be640c57571519aef07336edcbb78700
jkemboe <jkemboe@gmail.com>
2024-03-15 21:22:45 +0300

    Category Component Update

--- src/components/admin.js
+++ src/components/admin.js
@@ -1,15 +1,50 @@
 import Quill from "quill"
 import { useEffect, useState } from "react"
 import axios from "../lib/axios"
+import CategorySubComponent from "./admin/subcomponents/category"
+import { Link } from "react-router-dom"
 
 const AdminComponent = () => {
   
+    const [activeComponent, setActiveComponent] = useState('')
+
     return (
-        <div className="card">
-            <RoleComponent/>
-            <CategoryComponent/>
-            <NewArticleComponent/>
+        <div className="card container border-0">
+            <AdminNavigation setActiveComponent={setActiveComponent}/>
+            {activeComponent == "" && <WelcomeMessage/>}
+            {activeComponent == "roles" && <RoleComponent/>}
+            {activeComponent == "category" && <CategoryComponent/>}
+            {activeComponent == "articles" && <NewArticleComponent/>} 
         </div>
+    )
+}
+
+const AdminNavigation = ({ setActiveComponent }) => {
+    const handleNavChange = e => setActiveComponent(e.target.name)
+    return (
+        <nav className="m-2 p-2 d-flex justify-content-start">
+            <button 
+                className="btn btn-primary rounded-0 m-1"
+                onClick={handleNavChange}
+                name="roles"
+            >
+                    Roles
+            </button>
+            <button 
+                className="btn btn-primary rounded-0 m-1"
+                onClick={handleNavChange}
+                name="category"
+            >
+                Category
+            </button>
+            <button
+                className="btn btn-primary rounded-0 m-1"
+                onClick={handleNavChange}
+                name="articles"
+            >
+                Articles
+            </button>
+        </nav>
     )
 }
 
@@ -32,7 +67,8 @@ const RoleComponent = () => {
      
     }
     return (
-        <div className="card m-2">
+        <div className="d-flex row flex-row card">
+            <div className="col-6">
             <div className="card card-header">
                 <h6>Add Role</h6>
             </div>
@@ -54,56 +90,30 @@ const RoleComponent = () => {
                 </div>
                 
             </div>
-            <div className="card card-header">
-                <h6>Assign Role</h6>
+            
             </div>
+             <div className="col-6"> 
+                <div className="card card-header">
+                    <h6>Assign Role</h6>
+                </div>
+                <div className="card card-body">
+                    <input type="text" placeholder="assign role" className="form-control" />
+                </div>
+                <div className="card card-footer">
+                    <button className="btn btn-primary">
+                        Submit
+                    </button>
+                </div>
+             
+             </div>
         </div>
     )
 }
 
-
 const CategoryComponent = () => {
-    const [category, setCategory] = useState({
-        categoryName: ''
-    }) 
-
-    const onchange = e => setCategory({...category, [e.target.name]: e.target.value})
-    
-    const onsubmit = async () => {
-        try {
-        const { status } = await axios.post("/api/v1/category", category)
-        if (status == 201) {
-            alert("added")
-        } 
-        } catch (error) {
-            console.error(error)
-        }
-     
-    }
+  
     return (
-        <div className="card m-2">
-            <div className="card card-header">
-                <h6>Add Category</h6>
-            </div>
-            <div className="card card-body">
-                <input 
-                    type="text" 
-                    className="form-control border-secondary" 
-                    placeholder="enter category"
-                    name="categoryName"
-                    onChange={onchange}
-                />
-                <div>
-                    <button 
-                        className="btn btn-primary mt-2"
-                        onClick={onsubmit}
-                    >
-                        Submit
-                    </button>
-                </div>
-                
-            </div>
-        </div>
+       <CategorySubComponent/>
     )
 }
 
@@ -186,6 +196,27 @@ const NewArticleComponent = () => {
                 <div className="text-center">
                     <button className="btn btn-primary" onClick={onsubmit}>Post Article</button>
                 </div>
+            </div>
+        </div>
+    )
+}
+
+const WelcomeMessage = () => {
+    return (
+        <div className="card border-0">
+            <div className="card card-header bg-white border-0">
+                <h4 className="fw-bold">
+                
+                    ADMIN DASHBOARD
+                </h4>
+                <hr className="text-dark"/>
+            </div>
+            <div className="card card-body border-0 ">
+                <p className="text-secondary">Admin dashboard</p>
+                <p>Please click one of the navigation links above to get started</p>
+            </div>
+            <div className="card card-footer bg-white p-3">
+                <p>If you have any questions please leave us a message <Link to="/contact-us">HERE</Link></p>
             </div>
         </div>
     )
